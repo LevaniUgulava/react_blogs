3aec6bdc39f8ecf6054d2e27431c50c8e9a61281
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2024-05-05 20:33:55 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Product from './components/Product';
 import { fetchData, handleMassDelete, handleToggleSelect } from './helpers/helpers';
 
 function App() {
   const [selectedIds, setSelectedIds] = useState([]);
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetchData(setProducts);
+        fetchData(setProducts).then(() => setIsLoading(false));
     }, []);
     
 
@@ -30,24 +30,28 @@ function App() {
             </nav>
             <div className="container mx-auto p-4">
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {products && products?.map((product) => (
-                        <div className="col-span-1 relative" key={product.id}>
-                        <div className="border border-gray-200 rounded-lg shadow-lg overflow-hidden">
-                            <div className="p-5">
-                                <div className=" flex justify-center ">
-                                <div className="text-center">
-                                         <p>{product.sku}</p>
-                                        <p>{product.name}</p>
-                                        <p>{product.price} $</p>
-                                    </div>
-                                </div>
+                {isLoading ? (
+                  <div>Loading...</div>
+                ) : (
+                  products.length > 0 ? products.map((product) => (
+                    <div className="col-span-1 relative" key={product.id}>
+                      <div className="border border-gray-200 rounded-lg shadow-lg overflow-hidden">
+                        <div className="p-5">
+                          <div className=" flex justify-center ">
+                            <div className="text-center">
+                              <p>{product.sku}</p>
+                              <p>{product.name}</p>
+                              <p>{product.price} $</p>
                             </div>
+                          </div>
                         </div>
-                        <div className="absolute left-5 top-4">
-                            <input type="checkbox" className="delete-checkbox" onChange={() => handleToggleSelect(product.id,setSelectedIds)}/>
+                      </div>
+                <div className="absolute left-5 top-4">
+                          <input type="checkbox" className="delete-checkbox" onChange={() => handleToggleSelect(product.id, setSelectedIds)} />
                         </div>
-                    </div>
-                    ))}
+                      </div>
+                    )) : <div>No products found.</div>
+                  )}
                 </div>
             </div>
         </>
